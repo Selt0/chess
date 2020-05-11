dea8f53fd5e62b3197a61eea89983075c2b472a2
Selt0 <chewedgum12@gmail.com>
2020-05-11 13:12:21 -0500

    Clean up syntax

--- display.rb
+++ display.rb
@@ -1,5 +1,5 @@
-require 'colorize'
-require_relative 'cursor'
+require "colorize"
+require_relative "cursor"
 
 class Display
 
@@ -7,24 +7,12 @@ class Display
 
   def initialize(board)
     @board = board
-    @cursor = Cursor.new([0,0], board)
+    @cursor = Cursor.new([0, 0], board)
     @notifications = {}
-  end
-  
-  def reset!
-    @notifications.delete(:error)
-  end
-
-  def uncheck!
-    @notifications.delete(:check)
-  end
-
-  def set_check!
-    @notifications[:check] = 'Check!'
   end
 
   def build_grid
-    @board.grid.map.with_index do |row, i|
+    @board.rows.map.with_index do |row, i|
       build_row(row, i)
     end
   end
@@ -39,7 +27,7 @@ class Display
   def colors_for(i, j)
     if cursor.cursor_pos == [i, j] && cursor.selected
       bg = :light_green
-    elsif cursor.cursor_pos == [i,j]
+    elsif cursor.cursor_pos == [i, j]
       bg = :light_red
     elsif (i + j).odd?
       bg = :light_blue
@@ -49,9 +37,21 @@ class Display
     { background: bg }
   end
 
+  def reset!
+    @notifications.delete(:error)
+  end
+
+  def uncheck!
+    @notifications.delete(:check)
+  end
+
+  def set_check!
+    @notifications[:check] = "Check!"
+  end
+
   def render
     system("clear")
-    puts "Arrow keys, WASD, or vim to move. Space or enter to confirm"
+    puts "Arrow keys, WASD, or vim to move, space or enter to confirm."
     build_grid.each { |row| puts row.join }
 
     @notifications.each do |_key, val|
